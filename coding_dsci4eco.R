4a78760422a1f2b72be7d17e9a0f76bbb0a9a82c
trick track triu <tricktracktriu@mailbox.org>
2021-01-25 12:07:23 +0100

    new script

---
+++ coding_dsci4eco.R
@@ -0,0 +1,18 @@
+#####################
+## coding dsci4eco ##
+#####################
+
+
+# https://github.com/uo-ec510-2020-spring/lectures
+
+
+### pre-task: r language basics
+
+# https://raw.githack.com/uo-ec607/lectures/master/04-rlang/04-rlang.html#1
+# https://github.com/uo-ec607/lectures/tree/master/04-rlang
+
+
+
+
+
+
